623d302ceb456dea84bdf805c5868f27c5358b21
alumno44 <alumno44@examenTrim2.daw>
2023-03-12 21:46:54 +0000

    vers-2

--- nombreJava44.java
+++ nombreJava44.java
@@ -1,7 +1,7 @@
 public class nombreJava44 {
 
 	public static void main(String[] args) {
-		System.out.println("Me llamo Miroslav Yordanov Zhelev y he creado el servidor ubunserver44");
+		System.out.println("Me llamo Miroslav Yordanov Zhelev y he creado el servidor ubunserver44 y vivo en Torrejón de Ardoz");
 	}
 
 }
